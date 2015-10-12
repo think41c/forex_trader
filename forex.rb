da737d842faaf40083363a68829405feb84404bc
Tony DiNitto <think41c@gmail.com>
2015-10-12 11:41:19 -0500

    Variable name fixes and range corrections.

--- forex.rb
+++ forex.rb
@@ -12,13 +12,13 @@ class Forex
     puts "winning streak, or never a streak more than 1 winning trade at a time. It will then show with all the potential combinations"
     puts "what the highest amount you could win, as well as the lowest amount you could lose. As the law of big numbers implies, long"
     puts "streaks are possible in any direction, which is the failure of martingale systems."
-    puts "How many trades would you like to simulate? 0-20 is allowed."
+    puts "How many trades would you like to simulate? 10-20 is allowed."
     trades = gets.chomp 
     puts "What percentage of trades will you win? 0-100 is allowed."
     percentage = gets.chomp
     puts "We presume the exit is equadistant on both the winning and losing exit, therefore profit amounts are same loss amounts."
     puts "What's your initial win/loss dollar amount?"
-    win_amount = gets.chomp
+    amount = gets.chomp
 
   end
 end
