6d563e0591267086b9ec8b5088b41bc753f4ff80
Tony DiNitto <think41c@gmail.com>
2015-10-13 22:29:19 -0500

    Separated the intro screen.

--- forex.rb
+++ forex.rb
@@ -5,13 +5,17 @@ class Forex
     menu
   end
 
-  def menu
+  def intro 
     puts "This program goes through every possible combination of trades for you."
     puts "You can enter your probability of a successful trade, say 50%, and the program will simulate"
     puts "each combination of X amount of trades that would still provide that probability. Such that in 10 trades you might have 5x"
     puts "winning streak, or never a streak more than 1 winning trade at a time. It will then show with all the potential combinations"
     puts "what the highest amount you could win, as well as the lowest amount you could lose. As the law of big numbers implies, long"
     puts "streaks are possible in any direction, which is the failure of martingale systems."
+  end
+
+  def menu
+    intro
     puts "How many trades would you like to simulate? 10-20 is allowed."
     trades = gets.chomp 
     puts "What percentage of trades will you win? 0-100 is allowed."
@@ -29,7 +33,7 @@ class Forex
     lose_fix_or_perc = gets.chomp
     puts "What amount more or less would it be? (-100 to infinite for percentage or any infinite for fixed)"
     loser_change = gets.chomp
-    
+    puts "Remember the eur/usd and usd/chf are basically the same. AUD/USD & NZD/USD are same too."
   end
 end
 
