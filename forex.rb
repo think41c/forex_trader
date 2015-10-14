460ff1e468ff87e0be7eadba7eeda173404f6dfd
Tony DiNitto <think41c@gmail.com>
2015-10-13 22:30:47 -0500

    Separated out the user input processes.

--- forex.rb
+++ forex.rb
@@ -12,28 +12,33 @@ class Forex
     puts "winning streak, or never a streak more than 1 winning trade at a time. It will then show with all the potential combinations"
     puts "what the highest amount you could win, as well as the lowest amount you could lose. As the law of big numbers implies, long"
     puts "streaks are possible in any direction, which is the failure of martingale systems."
+    puts "Remember the eur/usd and usd/chf are basically the same. AUD/USD & NZD/USD are same too."
   end
 
-  def menu
-    intro
+  def get_user_data
     puts "How many trades would you like to simulate? 10-20 is allowed."
-    trades = gets.chomp 
+    @trades = gets.chomp 
     puts "What percentage of trades will you win? 0-100 is allowed."
-    percentage = gets.chomp
+    @percentage = gets.chomp
     puts "We presume the exit is equadistant on both the winning and losing exit, therefore profit amounts are same loss amounts."
     puts "What's your initial win/loss dollar amount? 0-100 is allowed."
-    amount = gets.chomp
+    @amount = gets.chomp
     puts "If you have a winning trade, would you like your next position size to be based on a percentage or a fixed amount more or less?"
     puts "(F)ixed or (P)ercentage"
     win_fix_or_perc = gets.chomp
     puts "What amount more or less would it be? (-100 to infinite for percentage or any infinite for fixed)"
-    winner_change = gets.chomp
+    @winner_change = gets.chomp
     puts "If you have a LOSING trade, would you like your next position size to be based on a percentage or a fixed amount more or less?"
     puts "(F)ixed or (P)ercentage"
-    lose_fix_or_perc = gets.chomp
+    @lose_fix_or_perc = gets.chomp
     puts "What amount more or less would it be? (-100 to infinite for percentage or any infinite for fixed)"
-    loser_change = gets.chomp
-    puts "Remember the eur/usd and usd/chf are basically the same. AUD/USD & NZD/USD are same too."
+    @loser_change = gets.chomp
+  end
+
+  def menu
+    intro
+    get_user_data
+   
   end
 end
 
