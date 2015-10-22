652add3679875b7e94ebb9ea08f2246194723801
Tony DiNitto <think41c@gmail.com>
2015-10-21 23:02:47 -0500

    Changed random figures to adhere to percentage win rate.

--- trade_creator.rb
+++ trade_creator.rb
@@ -1,7 +1,7 @@
 class TradeCreator
 	# Just use probabilities to calculate the win or loss for that trade. 
-	a = rand(1..2)
-	if a == 1 
+	a = rand(1..100)
+	if a < 80    # Percentage of winning trades.
 		trade = 1
 	else
 		trade = -1
