6aa9f36d7518b66937215254605b2879afa5dba7
Tony DiNitto <think41c@gmail.com>
2015-10-22 23:12:36 -0500

    Accurate percentage verification established.

--- trade_creator.rb
+++ trade_creator.rb
@@ -1,15 +1,35 @@
 class TradeCreator
 	# Just use probabilities to calculate the win or loss for that trade. 
-	
-	answers = []
-	10.times do 
-		a = rand(1..100)
-		if a < 50    # Percentage of winning trades.
-			trade = 1
-		else
-			trade = -1
+	def trade_gen
+		answers = []
+		10.times do 
+			a = rand(1..100)
+			if a < 50    # Percentage of winning trades.
+				trade = 1
+			else
+				trade = -1
+			end
+			answers << trade
 		end
-		answers << trade
+		puts answers
+		verify_percentage(answers)
 	end
-	puts answers
+
+	def verify_percentage(answers)
+		win  = 0
+		lose = 0 
+		answers.each do |x| 
+			if x == -1
+				lose += 1
+			else
+				win  += 1
+			end
+		end
+		total = win + lose
+		percentage = (win / total.to_f) * 100 
+		puts "The percentage of winners is #{percentage.to_i}%"
+	end
 end
+a = TradeCreator.new
+a.trade_gen
+a.trade_gen
