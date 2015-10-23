7d5bbb048905ec0f4da36efc22b1bceae81a6776
Tony DiNitto <think41c@gmail.com>
2015-10-22 23:06:25 -0500

    Creates an array that shows the win/losers.

--- trade_creator.rb
+++ trade_creator.rb
@@ -1,10 +1,15 @@
 class TradeCreator
 	# Just use probabilities to calculate the win or loss for that trade. 
-	a = rand(1..100)
-	if a < 80    # Percentage of winning trades.
-		trade = 1
-	else
-		trade = -1
+	
+	answers = []
+	10.times do 
+		a = rand(1..100)
+		if a < 50    # Percentage of winning trades.
+			trade = 1
+		else
+			trade = -1
+		end
+		answers << trade
 	end
-	puts trade
+	puts answers
 end
