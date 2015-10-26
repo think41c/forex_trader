e472014c3ac437eddecbbdfa3f0e7dfe9aff9644
Tony DiNitto <think41c@gmail.com>
2015-10-25 23:11:29 -0500

    New version based on probaabilites and 10,000 trades is being set up.

---
+++ probability_trader.rb
@@ -0,0 +1,19 @@
+# This revision of forex.rb doesn't necessitate all possible trades, but simply 10,000 trades using the probability given.
+require './trade_creator.rb'
+
+class Probability
+
+  def initialize 
+    @get_sequences = TradeCreator.new
+    puts "Welcome to Forex trader"
+    menu
+  end
+
+  def menu
+  	puts "Probability trader takes 10,000 trades based on your percentage."
+  end
+
+end
+
+a = Probability.new
+a
