35b527c8930ce8c5aa2d8a027ebb3b0a945bd44e
Tony DiNitto <think41c@gmail.com>
2015-12-18 17:40:20 -0600

    Calculations being established for drawdown calc.

--- drawdown.rb
+++ drawdown.rb
@@ -4,7 +4,15 @@ class Drawdown
 	end
 
 	def calculate
+		high     = 0
+		low      = 0
+		drawdown = 0
+
 		puts "The numbers to loko at are #{@numbers}"
+		@numbers.each do |x|
+			puts "looking at #{x}"
+		end
+
 	end
 end
 
