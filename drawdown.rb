35a80af6c1b3014c3e63be9044395dc8194e4624
Tony DiNitto <think41c@gmail.com>
2015-12-23 23:28:43 -0600

    Logic needs refinement - pseudocode added but needs to be fleshed out.

--- drawdown.rb
+++ drawdown.rb
@@ -7,24 +7,28 @@ class Drawdown
 		high     = 0
 		low      = @numbers[0]
 		drawdown = 0
+    new_high = false
 
 		puts "The numbers to loko at are #{@numbers}"
 		@numbers.each do |x|
 			puts "looking at #{x}"
 			if x > high
 			  high = x
+        new_high = true  # every number after this that isn't a) a new high and b) lower than this number should
+                         # be subtracted out for drawdown.
       end
       
       if x < high  
         low = x
       end
+
       puts "Current low is #{low} and the high is #{high}"
-       
+      puts "Drawdown is #{high-low}"
 
 		end
 
 	end
 end
 
-a = Drawdown.new([1,3,2])
+a = Drawdown.new([1,4,0])
 a.calculate
