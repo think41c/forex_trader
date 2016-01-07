10f07538b8e960ead65a05c30480fbb5c6db4593
Tony DiNitto <think41c@gmail.com>
2016-01-06 23:09:24 -0600

    Drawdown now returned from method.

--- drawdown.rb
+++ drawdown.rb
@@ -1,23 +1,19 @@
 class Drawdown
-	def initialize(numbers)
-		@numbers = numbers
-	end
-
-	def calculate
-		high     = 0
-    biggest_draw = 0
-
-		@numbers.each do |x|
+	def calculate(numbers)
+		high         = 0
+    drawdown = 0
+		numbers.each do |x|
 			high = x if x > high
       if x < high
-        if high-x > biggest_draw
-          biggest_draw = high-x
+        if high-x > drawdown
+          drawdown = high-x
         end
       end
 		end
-    puts biggest_draw
+    drawdown
 	end
 end
 
-a = Drawdown.new([1,4,0,5,0,2,3,6,0,500,400,900,100,700,0,0,1000,900,11000,10000])
-a.calculate
+a = Drawdown.new
+puts a.calculate([1,4,0,5,0,2,3,6,0,500,400,900,100,700,0,0,1000,900,11000,10000])
+
